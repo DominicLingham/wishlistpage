a6b953f5d289171d88563c07a90ddabd2b1d9fcf
Dominic Lingham <103593100+DominicLingham@users.noreply.github.com>
2024-02-05 21:28:43 +0000

    add category emoji picker and slight styling tweaks

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ const urlInput = document.getElementById("item-url");
 const nameInput = document.getElementById("item-name-input");
 const priceInput = document.getElementById("item-price-input");
 const quantityInput = document.getElementById("item-quantity-input");
-const items = [];
+const categoryInput = document.getElementById("category-picker");
+const wishlistEntries = [];
 let itemId = 1;
 
 const getInputValues = () => {
   //Gets the user's inputted values
   let urlVal = urlInput.value;
   let nameVal = nameInput.value;
-  let priceVal = "£" + priceInput.value.toString();
+  let priceVal = priceInput.value;
   let quantityVal = quantityInput.value;
+  let categoryVal = categoryInput.value;
+  let lineTotal = priceVal * quantityVal;
+
+  console.log(priceVal, quantityVal, lineTotal);
 
   //Ensures url is not considered a relative link
   if (!urlVal.startsWith("http://") && !urlVal.startsWith("https://")) {
@@ -22,8 +27,10 @@ const getInputValues = () => {
   let obj = {
     url: urlVal,
     name: nameVal,
-    price: priceVal,
-    quantity: quantityVal,
+    price: priceVal.toString(),
+    quantity: quantityVal.toString(),
+    category: categoryVal,
+    totalCost: lineTotal.toString(),
   };
 
   return obj;
@@ -38,17 +45,22 @@ const addItem = (e) => {
   e.preventDefault();
   const itemObject = getInputValues();
 
+  wishlistEntries.push(itemObject);
+
   let isValid = validateInputs(itemObject);
 
   if (isValid) {
     wishlistItems.innerHTML += `
     <div class="item" data-item-id=${itemId}>
     <a href="${itemObject.url}" target="_blank">
+    <span class="category-icon">${itemObject.category}</span>
       <p class="item-title">${itemObject.name}</p>
 
-      <p class="item-price">${itemObject.price}</p>
+      <p class="item-price">£${itemObject.price}</p>
 
       <p class="item-quantity">${itemObject.quantity}</p>
+
+      <p class="total-line-price">£${itemObject.totalCost}</p>
     </a>
     <button class="remove-btn" onclick="removeItem(${itemId})">Remove item</button>
   </div>
